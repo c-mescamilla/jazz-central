8bd45e04a59389c210a325b52026636b953bd711
c-mgonzalez <106987161+c-mgonzalez@users.noreply.github.com>
2022-06-27 11:19:13 -0600

    Login v2
    the reset password scenario was added

--- cypress/integration/login/login_steps.js
+++ cypress/integration/login/login_steps.js
@@ -20,9 +20,12 @@ Then("A user clicks on Logout button", () => {
     loginPage.clickLogout()
 })
 
-/*
+
 When('A user clicks on Forgot password button', () => {
     loginPage.clickResetBtn()
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
 })
 
 And('Verify user will {string} message', (textMessage) => {
@@ -37,4 +40,8 @@ Then('A user enter email', (table) => {
 
 And("A user clicks on Reset Password button", () => {
     loginPage.clickResetPasswordBtn()
-})*/
+})
+
+And('Verify user receiving {string} message', (textMessage) => {
+    loginPage.elements.resetSendMessage().should('contain', textMessage)
+})
