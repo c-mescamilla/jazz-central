8d4119405d27b9b418c4a7aa9df48d7193490e9e
Edwin Arroyo <c-edwin.arroyo@cart.com>
2022-06-28 10:13:46 -0500

    Edwin Updates

--- cypress/integration/tenant/tenant_steps.js
+++ cypress/integration/tenant/tenant_steps.js
@@ -3,12 +3,22 @@ import { Before, Given, When, Then, And } from "cypress-cucumber-preprocessor/st
 
 And("I click on tenant dropdown", () => {
     //TEA Living
-    cy.get('#navbar-list-tenant').click({force: true})
-    cy.wait(500)
+    // cy.get('#view-as-tenant-select').find('option').each(($el, index) => {
+    //     cy.get('#view-as-tenant-select').select(index, { timeout: 110000 }).should('not.be.undefined')
+    //     cy.wait(5000)
+    // })
+    cy.get('#view-as-tenant-select').find('option').then(listLength => {
+        if (listLength.length == listLength.length) {
+            cy.log('The number of the list is: ' + listLength.length)
+        }
+        //cy.get('#view-as-tenant select').find('option').its('length').should('have.length', listLength)
+    })
+
+    //cy.get('#view-as-tenant-select').find('option').should('have.length',109)
 })
 
-And("I select child tenant ", () => {
-    //TEA Living
-    cy.get('#navbar-list-tenant').select('Zenfuel')
-    cy.wait(500)
-})
+// And("I select child tenant ", () => {
+//     //TEA Living
+//     cy.get('#navbar-list-tenant').select('Zenfuel')
+//     cy.wait(500)
+// })
